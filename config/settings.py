809c8facc27d9c551b9c8d1e632cc30abcae8b13
a-babushkin <a.babushkin42@gmail.com>
2025-09-26 01:23:34 +0300

    Add postgres to test

--- config/settings.py
+++ config/settings.py
@@ -115,7 +115,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [ '/app/static']
+# STATICFILES_DIRS = [ '/app/static']
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
