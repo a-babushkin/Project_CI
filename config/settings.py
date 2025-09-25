bdb399ee6781175880a42d6cc1f2c34771781ecb
a-babushkin <a.babushkin42@gmail.com>
2025-09-26 01:39:48 +0300

    Correct small

--- config/settings.py
+++ config/settings.py
@@ -115,7 +115,6 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "static/"
-# STATICFILES_DIRS = [ '/app/static']
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
