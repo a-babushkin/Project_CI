dc47a333954af0ebddebe4e426b3570be30fe005
a-babushkin <a.babushkin42@gmail.com>
2025-09-27 15:54:46 +0300

    010

--- config/settings.py
+++ config/settings.py
@@ -165,5 +165,5 @@ if TESTING:
     CELERY_RESULT_BACKEND = "cache+memory://"
 else:
     # ваша обычная конфигурация
-    CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL", "redis://127.0.0.1:6379/0")
-    CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND", "redis://127.0.0.1:6379/0")
+    CELERY_BROKER_URL = os.getenv("REDIS_LOCATION")
+    CELERY_RESULT_BACKEND = os.getenv("REDIS_LOCATION")
